211f77fe9b6a6354c575bd3ea8e9a5805b6b8f22
Janice Libby <118792927+JaniceLibbyThomas@users.noreply.github.com>
2023-02-14 11:52:34 +0530

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -3,7 +3,7 @@ from sqlite3 import connect
 import streamlit as st
 
 conn = connect("stocks.db")
-data = pd.read_csv("/Users/joelsanthoshraja/Downloads/Secrets.csv")
+data = pd.read_csv("https://raw.githubusercontent.com/JaniceLibbyThomas/dummy1/main/Secrets.csv")
 data.to_sql("meta",conn)
 df = pd.read_sql("SELECT * FROM meta",conn)
 st.dataframe(df)
